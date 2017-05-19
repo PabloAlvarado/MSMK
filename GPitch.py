1b8dbaf8a0b55cfcd466a8889873c05596ef97aa
Pablo A. Alvarado <p.a.alvaradoduran@qmul.ac.uk>
2017-05-19 17:33:22 +0100

    Update GPitch.py

--- GPitch.py
+++ GPitch.py
@@ -1,4 +1,4 @@
-# Copyright 2016 Pablo A. Alvarado
+# Copyright 2017 Pablo A. Alvarado
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
